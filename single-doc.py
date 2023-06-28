75bc2238142a5462c7acbb6ca9a6b43c99e0ef78
Stephen Mooney <stephen@tecton.ai>
2023-06-28 11:55:19 -0400

    initial commit

---
+++ single-doc.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+
+from dotenv import load_dotenv
+from langchain.llms import OpenAI
+from langchain.chains.question_answering import load_qa_chain
+from langchain.document_loaders import PyPDFLoader
+
+load_dotenv('.env')
+
+pdf_loader = PyPDFLoader('./docs/RachelGreenCV.pdf')
+documents = pdf_loader.load()
+
+chain = load_qa_chain(llm=OpenAI())
+query = 'Who is the CV about?'
+response = chain.run(input_documents=documents, question=query)
+print(response)
+
+query = 'What is the candidates highest level of education?'
+response = chain.run(input_documents=documents, question=query)
+print(response)
